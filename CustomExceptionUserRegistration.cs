b53d541189c50d4fcac4e08b1437f84c064f8c66
firsttoken <monikasharma12330@gmail.com>
2023-02-07 15:17:51 +0530

    UC12 implement exception in User registration

---
+++ CustomExceptionUserRegistration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationRegex
+{
+    public  class CustomExceptionUserRegistration:Exception
+    {
+        public enum ExceptionType
+        {
+            INVALID_NAME,INVALID_EMAIL,INVALID_PHONE,INVALID_PASSWORD_MIN8CHAR_1UPPER,NULL_MESSAGE
+        }
+        private readonly ExceptionType type;   
+        public CustomExceptionUserRegistration(ExceptionType type,string message):base(message)
+        {
+            this.type= type;
+        }
+    }
+}
